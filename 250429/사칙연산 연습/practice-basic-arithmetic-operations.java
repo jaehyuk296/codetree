d45792fafed36f7ea94d92738fa5709a684b4d6c
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-04-29 11:24:34 +0900

    Title: 사칙연산 연습, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250429/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a=5;
+        int b=30;
+        int c= a+b;
+        int ans1 = c/8;
+        int ans2 = c%8;
+        System.out.println(ans1);
+        System.out.println(ans2);
+        
+    }
+}
