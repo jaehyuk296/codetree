4a0eaab9dc8c847f8194a1a98149eddeaba52ec8
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-05-09 10:38:13 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 110ms, Memory: 10MB, Status: Passed - Codetree

--- 250509/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
+++ 250509/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -1,27 +1,21 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String text = sc.next();
         String pattern = sc.next();
-        // Please write your code here.
-       
-        System.out.println( find(text,pattern));
+
+        System.out.println(find(text, pattern));
     }
 
-    public static int find(String a,String pattern){
-        int size = a.length();
-        String pat [] = pattern.split("");
-        String alp [] = a.split("");
-        for(int i=0;i<size-1;i++){
-            for(int j=0;j<pattern.length()-1;j++){
-                if(alp[i].equals(pat[j])){
-                    if(alp[i+1].equals(pat[j+1])){
-                        int cnt=i;
-                        return cnt;
-                    }
-                    break;
-                }
+    public static int find(String text, String pattern) {
+        int textLen = text.length();
+        int patternLen = pattern.length();
+
+        for (int i = 0; i <= textLen - patternLen; i++) {
+            if (text.substring(i, i + patternLen).equals(pattern)) {
+                return i;
             }
         }
         return -1;
