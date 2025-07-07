86b1b423e615f0f0b040747b32ad6c28f85c25c8
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-07-07 15:19:49 +0900

    Title: 폭탄 해제, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250707/폭탄 해제/bomb-removal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Bomb{
+    String code;
+    char rgb;
+    int second;
+
+    public Bomb(String code, char rgb, int second){
+        this.code=code;
+        this.rgb=rgb;
+        this.second=second;
+    }
+
+    public void print(){
+        System.out.println("code : "+);
+        System.out.println("code : "+);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String uCode = sc.next();
+        char lColor = sc.next().charAt(0);
+        int time = sc.nextInt();
+        // Please write your code here.
+    }
+}
