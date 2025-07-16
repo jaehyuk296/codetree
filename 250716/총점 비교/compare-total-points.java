c8a74a3114065cf55ddc417402b0f4214fcea3a1
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-07-16 16:28:01 +0900

    Title: 총점 비교, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250716/총점 비교/compare-total-points.java
+++ 250716/총점 비교/compare-total-points.java
@@ -1,13 +1,21 @@
 import java.util.*;
 
-class Student implements Comparalbe <Student>{
+class Student implements Comparable<Student> {
     String name;
-    int score1,score2,score3;
+    int score1, score2, score3;
+    int totalScore;
 
-    public Student(String name,int score1,int score2,int score3){
-        this.name=name;
-        this.score1=score1;
-        this.score1=score1;
+    public Student(String name, int score1, int score2, int score3) {
+        this.name = name;
+        this.score1 = score1;
+        this.score2 = score2;
+        this.score3 = score3;
+        this.totalScore = score1 + score2 + score3;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return this.totalScore - student.totalScore;
     }
 }
 
@@ -15,12 +23,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        Student[] student = new Student[n];
+
         for (int i = 0; i < n; i++) {
             String name = sc.next();
             int score1 = sc.nextInt();
             int score2 = sc.nextInt();
             int score3 = sc.nextInt();
+            student[i] = new Student(name, score1, score2, score3);
         }
-        // Please write your code here.
+
+        Arrays.sort(student);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(student[i].name + " " +
+                               student[i].score1 + " " +
+                               student[i].score2 + " " +
+                               student[i].score3 + " " );
+        }
     }
 }
