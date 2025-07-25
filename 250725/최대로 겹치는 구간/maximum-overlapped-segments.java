ee9dda7119a8195d38ba33001a6afc989cadc0e6
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-07-25 13:19:37 +0900

    Title: 최대로 겹치는 구간, Time: 128ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250725/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 250725/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -1,27 +1,36 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] x1 = new int[n];
         int[] x2 = new int[n];
+
+        int maxIndex = 0;
         for (int i = 0; i < n; i++) {
             x1[i] = sc.nextInt();
             x2[i] = sc.nextInt();
+            if (x2[i] > maxIndex) {
+                maxIndex = x2[i];
+            }
         }
-        // Please write your code here.
-        int total [] = new int [100];
 
-        for(int i=0;i<n;i++){
-            for(int j=x1[i];j<x2[i];j++){
+        int[] total = new int[maxIndex + 1];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = x1[i]; j < x2[i]; j++) {
                 total[j]++;
             }
         }
-        int max=0;
-        for(int i=0;i<100;i++){
-            if(max<total[i])
+
+        int max = 0;
+        for (int i = 0; i < total.length; i++) {
+            if (total[i] > max) {
                 max = total[i];
+            }
         }
+
         System.out.print(max);
     }
 }
