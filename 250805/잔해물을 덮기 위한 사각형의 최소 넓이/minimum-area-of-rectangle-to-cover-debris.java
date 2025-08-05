9bfd16ee3e9d99f1049b0b8e779461792e4e0416
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-08-05 23:06:14 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 186ms, Memory: 26MB, Status: Passed - Codetree

--- 250805/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 250805/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -2,53 +2,55 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int offset = 1000;
         Scanner sc = new Scanner(System.in);
-        int rect1_x1 = sc.nextInt()+offset;
-        int rect1_y1 = sc.nextInt()+offset;
-        int rect1_x2 = sc.nextInt()+offset;
-        int rect1_y2 = sc.nextInt()+offset;
 
-        int rect2_x1 = sc.nextInt()+offset;
-        int rect2_y1 = sc.nextInt()+offset;
-        int rect2_x2 = sc.nextInt()+offset;
-        int rect2_y2 = sc.nextInt()+offset;
-        // Please write your code here.
-        int area [][] = new int [2001][2001];
-        for(int i=rect1_x1;i<rect1_x2;i++){
-            for(int j=rect1_y1;j<rect1_y2;j++){
-                area[i][j]=1;
+        int offset = 1000;
+        int[][] board = new int[2001][2001];
+
+        int x1 = sc.nextInt() + offset;
+        int y1 = sc.nextInt() + offset;
+        int x2 = sc.nextInt() + offset;
+        int y2 = sc.nextInt() + offset;
+
+        int x3 = sc.nextInt() + offset;
+        int y3 = sc.nextInt() + offset;
+        int x4 = sc.nextInt() + offset;
+        int y4 = sc.nextInt() + offset;
+
+        // 첫 번째 사각형을 1로 채움
+        for (int i = x1; i < x2; i++) {
+            for (int j = y1; j < y2; j++) {
+                board[i][j] = 1;
             }
         }
 
-        for(int i=rect2_x1;i<rect2_x2;i++){
-            for(int j=rect2_y1;j<rect2_y2;j++){
-                area[i][j]=0;
+        // 두 번째 사각형으로 덮기 (0으로 만듦)
+        for (int i = x3; i < x4; i++) {
+            for (int j = y3; j < y4; j++) {
+                board[i][j] = 0;
             }
-        }
-        int max=-1;
-        int ad = 0;
-
-        for(int i=0;i<2001;i++){
-            for(int j=0;j<2001;j++){
-                if(area[i][j]==1)
-                ad++;
-            }
-            if(max<ad)
-                max=ad;
-            ad=0;
         }
 
-        int max2=-1;
-        for(int i=0;i<2001;i++){
-            for(int j=0;j<2001;j++){
-                if(area[j][i]==1)
-                ad++;
+        // 남아있는 1들의 최소, 최대 좌표 찾기
+        int minX = 2001, minY = 2001;
+        int maxX = -1, maxY = -1;
+        for (int i = 0; i < 2001; i++) {
+            for (int j = 0; j < 2001; j++) {
+                if (board[i][j] == 1) {
+                    if (i < minX) minX = i;
+                    if (i > maxX) maxX = i;
+                    if (j < minY) minY = j;
+                    if (j > maxY) maxY = j;
+                }
             }
-            if(max2<ad)
-                max2=ad;
-            ad=0;
         }
-        System.out.println(max*max2);
+
+        // 넓이 계산
+        int area = 0;
+        if (maxX >= 0 && maxY >= 0) {
+            area = (maxX - minX + 1) * (maxY - minY + 1);
+        }
+
+        System.out.println(area);
     }
 }
