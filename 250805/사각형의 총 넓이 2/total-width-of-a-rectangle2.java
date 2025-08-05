132f83915c975e289efeb9a73c44e80df06880a4
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-08-05 12:08:02 +0900

    Title: 사각형의 총 넓이 2, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250805/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 250805/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -14,17 +14,18 @@ public class Main {
             y2[i] = sc.nextInt()+100;
         }
         // Please write your code here.
-        int offset [][] = new int [200][200];
-        for(int i=0;i<n;i++){
-            for(int j=x1[i];j<=x2[i];j++){
-                offset[j][y1[i]]=1;
-                offset[j][y2[i]]=1;
+        int grid [][] = new int [201][201];
+        for (int i = 0; i < n; i++) {
+            for (int x = x1[i]; x < x2[i]; x++) {
+                for (int y = y1[i]; y < y2[i]; y++) {
+                    grid[x][y] = 1;
+                }
             }
         }
         int w = 0;
-        for(int i=0;i<200;i++){
-            for(int j=0;j<200;j++){
-                if(offset[i][j]==1)
+        for(int i=0;i<201;i++){
+            for(int j=0;j<201;j++){
+                if(grid[i][j]==1)
                     w++;
             }
         }
