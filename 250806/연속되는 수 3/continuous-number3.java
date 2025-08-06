8f88e4d986733cf0f74716780d48a4d0d9a2da9e
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-08-06 15:23:43 +0900

    Title: 연속되는 수 3, Time: 166ms, Memory: 11MB, Status: Passed - Codetree

--- 250806/연속되는 수 3/continuous-number3.java
+++ 250806/연속되는 수 3/continuous-number3.java
@@ -4,37 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         int[] arr = new int[n];
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
-        
-        // Please write your code here.
-        int cnt =1;
-        int max =1;
-        boolean bool = true; //양수?
-        for(int i=0;i<n;i++){
-            if(bool){
-                if(arr[i]>0){
-                    cnt++;
-                }else{
-                    bool = false;
-                    if(cnt>max)
-                        max=cnt;
-                    cnt=1;
-                }
-            }else{
-                if(arr[i]<0){
-                    cnt++;
-                }else{
-                    bool = true;
-                    if(cnt>max)
-                        max=cnt;
-                    cnt=1;
-                }
+
+        int cnt = 1;
+        int max = 1;
+        boolean isPositive = arr[0] > 0;
+
+        for (int i = 1; i < n; i++) {
+            if ((arr[i] > 0 && isPositive) || (arr[i] < 0 && !isPositive)) {
+                cnt++;
+            } else {
+                max = Math.max(max, cnt);
+                cnt = 1;
+                isPositive = arr[i] > 0;
             }
-            if(cnt>max)
-                max = cnt;
         }
+
+        max = Math.max(max, cnt);  // 마지막 구간 체크
         System.out.println(max);
     }
 }
