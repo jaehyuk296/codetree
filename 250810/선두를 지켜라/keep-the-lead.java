c038b00056366ccd6a4cc4bad749a6afd096c50e
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-08-10 13:43:21 +0900

    Title: 선두를 지켜라, Time: 276ms, Memory: 17MB, Status: Passed - Codetree

--- 250810/선두를 지켜라/keep-the-lead.java
+++ 250810/선두를 지켜라/keep-the-lead.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // A의 구간 수
-        int m = sc.nextInt(); // B의 구간 수
+        int n = sc.nextInt(); // A 구간 수
+        int m = sc.nextInt(); // B 구간 수
 
         int[][] A = new int[n][2];
         for (int i = 0; i < n; i++) {
-            A[i][0] = sc.nextInt(); // 속도
-            A[i][1] = sc.nextInt(); // 시간
+            A[i][0] = sc.nextInt();
+            A[i][1] = sc.nextInt();
         }
 
         int[][] B = new int[m][2];
@@ -18,47 +18,52 @@ public class Main {
             B[i][1] = sc.nextInt();
         }
 
-        // 총 시간 계산 (A와 B의 총 시간은 같다고 가정)
         int totalTime = 0;
         for (int i = 0; i < n; i++) totalTime += A[i][1];
 
         int[] posA = new int[totalTime + 1];
         int[] posB = new int[totalTime + 1];
 
-        // A의 초 단위 위치 계산
         int time = 0;
         for (int i = 0; i < n; i++) {
-            int v = A[i][0];
-            int t = A[i][1];
+            int v = A[i][0], t = A[i][1];
             for (int j = 0; j < t; j++) {
                 time++;
                 posA[time] = posA[time - 1] + v;
             }
         }
 
-        // B의 초 단위 위치 계산
         time = 0;
         for (int i = 0; i < m; i++) {
-            int v = B[i][0];
-            int t = B[i][1];
+            int v = B[i][0], t = B[i][1];
             for (int j = 0; j < t; j++) {
                 time++;
                 posB[time] = posB[time - 1] + v;
             }
         }
 
-        // 선두 변화 카운트
         int cnt = 0;
-        String leader = "same"; // 처음엔 동률
-        for (int i = 1; i <= totalTime; i++) {
-            String nowLeader;
-            if (posA[i] > posB[i]) nowLeader = "A";
-            else if (posB[i] > posA[i]) nowLeader = "B";
-            else nowLeader = "same";
+        String lastRealLeader = "same";
 
-            if (!nowLeader.equals("same") && !nowLeader.equals(leader)) {
+        for (int i = 1; i <= totalTime; i++) {
+            String now;
+            if (posA[i] > posB[i]) now = "A";
+            else if (posB[i] > posA[i]) now = "B";
+            else now = "same";
+
+            if (now.equals("same")) {
+                continue;
+            }
+
+            if (lastRealLeader.equals("same")) {
+                // 첫 실제 선두가 등장한 순간: 카운트하지 않고 저장만
+                lastRealLeader = now;
+                continue;
+            }
+
+            if (!now.equals(lastRealLeader)) {
                 cnt++;
-                leader = nowLeader;
+                lastRealLeader = now;
             }
         }
 
