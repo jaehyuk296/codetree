26b79e26ff010aa7439fd7174faf947a63a7565d
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-08-14 20:02:58 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250814/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Main {
+    public static boolean inRange(int x,int y, int n,int m){
+        return(0<=x&&x<n&&0<=y&&y<m);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        // Please write your code here.
+        int square [][]= new int [n][m];
+        //상우하좌
+        int dx [] = {0,1,0,-1};
+        int dy [] = {1,0,-1,0};
+        int x=0,y=0;//시작점
+        square[0][0]=1;
+        int dirNum = 0;//오른쪽 방향으로
+
+        for(int i=2;i<=n*m;i++){
+            int nx = x+dx[dirNum], ny = y+dy[dirNum];
+
+            if(!inRange(nx,ny,n,m)||square[nx][ny]!=0)//범위를 벗어나거나 0이 아니면
+                dirNum = (dirNum+1)%4;
+
+            x=x+dx[dirNum];
+            y=y+dy[dirNum];
+            square[x][y]=i;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(square[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
