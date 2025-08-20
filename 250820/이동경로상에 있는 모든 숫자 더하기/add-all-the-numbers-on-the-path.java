d13f17633cd28a23fac29ca121187065abce49d0
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-08-20 12:07:21 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 269ms, Memory: 16MB, Status: Passed - Codetree

--- 250820/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.java
+++ 250820/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.java
@@ -3,42 +3,43 @@ import java.util.Scanner;
 public class Main {
     public static int n;
     public static boolean inRange(int x,int y){
-        return (0<=x&&x<n&&0<=y&&y<n);
+        return (0<=x && x<n && 0<=y && y<n);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();//사이즈 홀수
-        int t = sc.nextInt();//명령 개수
-        String commands = sc.next();//R,L,F
-        
+        n = sc.nextInt(); // 보드 크기(홀수)
+        int t = sc.nextInt(); // 명령 개수
+        String commands = sc.next(); // 명령 문자열 (R, L, F)
+
         int[][] board = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 board[i][j] = sc.nextInt();
             }
         }
-        // Please write your code here.
-        int x = n/2, y = n/2;//시작점 (center)
+
+        int x = n/2, y = n/2; // 시작점: 중앙
         int angle = 0; // 0: 북, 90: 동, 180: 남, 270: 서
-        int sum = 0;
-        
-        for(int i=0;i<t;i++){//명령어 비교
+        int sum = board[x][y]; // 시작 위치 값 포함
+
+        for(int i=0; i<t; i++) {
             char com = commands.charAt(i);
-            if(inRange(x,y))
-                sum += board[x][y];
-            else continue;
 
-            if(com=='L') angle=(angle+270)%360;
-            else if(com=='R') angle=(angle+90)%360;
+            if(com=='L') angle = (angle+270)%360;
+            else if(com=='R') angle = (angle+90)%360;
             else if(com=='F') {
+                int nx = x, ny = y;
                 switch(angle){
-                    case 0: x--;break;
-                    case 90: y++;break;
-                    case 180: x++;break;
-                    case 270: y--;break;
+                    case 0: nx = x-1; ny = y; break;   // 북
+                    case 90: nx = x; ny = y+1; break;  // 동
+                    case 180: nx = x+1; ny = y; break; // 남
+                    case 270: nx = x; ny = y-1; break; // 서
                 }
-
+                if(inRange(nx, ny)) {
+                    x = nx; y = ny;
+                    sum += board[x][y];
+                }
             }
         }
         System.out.println(sum);
