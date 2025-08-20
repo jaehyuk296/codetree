4cc6d83c3968bdf60562c49345cb5913248e4bdb
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-08-20 11:39:16 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250820/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n;
+    public static boolean inRange(int x,int y){
+        return (0<=x&&x<n&&0<=y&&y<n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();//사이즈 홀수
+        int t = sc.nextInt();//명령 개수
+        String commands = sc.next();//R,L,F
+        
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        // Please write your code here.
+        int x = n/2, y = n/2;//시작점 (center)
+        int angle = 0; // 0: 북, 90: 동, 180: 남, 270: 서
+        int sum = 0;
+        
+        for(int i=0;i<commands.length();i++){//명령어 비교
+            char com = commands.charAt(i);
+            if(com=='L') angle=(angle+270)%360;
+            else if(com=='R') angle=(angle+90)%360;
+            else if(com=='F') {
+                switch(angle){
+                    case 0: x++;break;
+                    case 0: x++;break;
+                }
+            }
+        }
+
+
+
+    }
+}
