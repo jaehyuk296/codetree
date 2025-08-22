84c8fcf1f9eb913d4be5a3662c57e3859d6c15f8
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-08-22 12:41:22 +0900

    Title: 이상한 진수 2, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 250822/이상한 진수 2/awkward-digits-2.java
+++ 250822/이상한 진수 2/awkward-digits-2.java
@@ -1,24 +1,29 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        // Please write your code here.
-        int num [] = new int [10];
-        for(int i=0;i<a.length();i++){
-            num[i] = a.charAt(i)-'0';
-        }
 
-        for(int i=1;i<a.length();i++){//a의 맨앞자리 0이 아님을 가정
-            if(num[i]==0){
-                num[i] = 1;
-                break;
+        int maxVal = 0;
+
+        for (int i = 0; i < a.length(); i++) {
+            // i번째 자리 flip
+            char[] arr = a.toCharArray();
+            arr[i] = (arr[i] == '0') ? '1' : '0';
+
+            // 2진수 → 10진수 변환
+            int val = 0;
+            for (int j = 0; j < arr.length; j++) {
+                val = val * 2 + (arr[j] - '0');
+            }
+
+            // 최대값 갱신
+            if (val > maxVal) {
+                maxVal = val;
             }
         }
-        int n = 0;
-        for(int i=0;i<a.length();i++){//2진수 10진수로 변환
-            n = n*2+num[i];
-        }
-        System.out.println(n);
+
+        System.out.println(maxVal);
     }
 }
