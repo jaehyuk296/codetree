0486c3cf4301b9cf4f3cd295012415a432c27ce3
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-08-25 16:11:02 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 250825/괄호 쌍 만들어주기 2/pair-parentheses-2.java
+++ 250825/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
+    public static String str;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-
-        int open = 0, close = 0;
-
-        for (int i = 0; i < input.length() - 1; i++) {
-            if (input.charAt(i) == '(' && input.charAt(i+1) == '(') {
-                open++;
-            }
-            if (input.charAt(i) == ')' && input.charAt(i+1) == ')') {
-                close++;
-            }
-        }
-
-        System.out.println(open * close);
+        // 입력
+        str = sc.next();
+        int n = (int) str.length();
+        
+        // 모든 쌍을 다 잡아봅니다.
+        int cnt = 0;
+        for(int i = 0; i < n - 1; i++)
+            for(int j = i + 1; j < n - 1; j++)
+                if(str.charAt(i) == '(' && str.charAt(i + 1) == '('
+                    && str.charAt(j) == ')' && str.charAt(j + 1) == ')')
+                    cnt++;
+        
+        System.out.print(cnt);
     }
 }
