b52bde53a4f8d2e667244d0e7f77eaaf04f27d4b
jaehyuk296 <150820266+jaehyuk296@users.noreply.github.com>
2025-08-25 16:05:51 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 117ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250825/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean check(char a, char b) {
+        return a == b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        int open = 0, close = 0;
+
+        for (int i = 0; i < input.length() - 1; i++) { // i+1까지 안전하게 접근
+            if (check(input.charAt(i), input.charAt(i+1))) {
+                if (input.charAt(i) == '(') {
+                    open++;
+                } else if (input.charAt(i) == ')') {
+                    close++;
+                }
+            }
+        }
+
+        System.out.println(open * close);
+    }
+}
